1b527c723b6066451db48a6320053f067f31622d
Tom Berger <tom.berger@gmail.com>
2015-03-20 11:55:21 +0100

    Oops Syntax Error.

--- aldryn_config.py
+++ aldryn_config.py
@@ -3,25 +3,23 @@
 from aldryn_client import forms
 
 class Form(forms.BaseForm):
-
     def to_settings(self, cleaned_data, settings_dict):
+        country_mw = 'country_segment.middleware.ResolveCountryCodeMiddleware'
+        if country_mw not in settings_dict['MIDDLEWARE_CLASSES']:
+            for position, mw in enumerate(settings_dict['MIDDLEWARE_CLASSES']):
+                #
+                # Its not a strict requirement that the
+                # ResolveCountryCodeMiddleware go after SessionMiddleware,
+                # but, it seems like a pretty nice place.
+                #
+                if mw == 'django.contrib.sessions.middleware.SessionMiddleware':
+                    settings_dict['MIDDLEWARE_CLASSES'].insert(position + 1, country_mw)
+                    break
+            else:
+                #
+                # B'okay, not sure how this CMS installation works, but...
+                # let's just put it at the top.
+                #
+                settings_dict['MIDDLEWARE_CLASSES'].insert(0, country_mw)
 
-    country_mw = 'country_segment.middleware.ResolveCountryCodeMiddleware'
-    if country_mw not in settings_dict['MIDDLEWARE_CLASSES']:
-        for position, mw in enumerate(settings_dict['MIDDLEWARE_CLASSES']):
-            #
-            # Its not a strict requirement that the
-            # ResolveCountryCodeMiddleware go after SessionMiddleware,
-            # but, it seems like a pretty nice place.
-            #
-            if mw == 'django.contrib.sessions.middleware.SessionMiddleware':
-                settings_dict['MIDDLEWARE_CLASSES'].insert(position + 1, country_mw)
-                break
-        else:
-            #
-            # B'okay, not sure how this CMS installation works, but...
-            # let's just put it at the top.
-            #
-            settings_dict['MIDDLEWARE_CLASSES'].insert(0, country_mw)
-
-    return settings_dict
+        return settings_dict
